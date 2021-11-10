2e6e49eb5385ee626405389b448a72251d0bce73
Boris Aguilar <baguilar@innovare.es>
2021-11-10 14:30:26 -0600

    Adding missing redirect in certbot

--- 5.39-team-edition-do/dockerbase.tf
+++ 5.39-team-edition-do/dockerbase.tf
@@ -83,7 +83,7 @@ resource "digitalocean_droplet" "www-mattermost" {
       "systemctl restart nginx",
       "ufw allow http",
       "ufw allow https",
-      "%{if var.domain!= ""}certbot --nginx --non-interactive --agree-tos --domains ${var.domain} %{if var.webmaster_email!= ""} --redirect --email ${var.webmaster_email} %{ else } --register-unsafely-without-email %{ endif } %{ else }echo NOCERTBOT%{ endif }"
+      "%{if var.domain!= ""}certbot --nginx --non-interactive --agree-tos --domains ${var.domain} --redirect %{if var.webmaster_email!= ""} --email ${var.webmaster_email} %{ else } --register-unsafely-without-email %{ endif } %{ else }echo NOCERTBOT%{ endif }"
     ]
   }
 }
