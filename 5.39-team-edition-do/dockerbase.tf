09921a7ff4e2134a944baf254423807108b34e8e
Boris Aguilar <baguilar@innovare.es>
2021-11-09 19:26:02 -0600

    Updating to support domain creation by default

--- 5.39-team-edition-do/dockerbase.tf
+++ 5.39-team-edition-do/dockerbase.tf
@@ -6,6 +6,10 @@ variable "domain" {
     default = ""
 }
 
+variable "webmaster_email" {
+    default = ""
+}
+
 resource "random_password" "database_password" {
   length           = 16
   special          = true
@@ -79,6 +83,7 @@ resource "digitalocean_droplet" "www-mattermost" {
       "systemctl restart nginx",
       "ufw allow http",
       "ufw allow https",
+      "%{if var.domain!= ""}certbot --nginx --non-interactive --agree-tos --domains ${var.domain} %{if var.webmaster_email!= ""} --redirect --email ${var.webmaster_email} %{ else } --register-unsafely-without-email %{ endif } %{ else }echo NOCERTBOT%{ endif }"
     ]
   }
 }
