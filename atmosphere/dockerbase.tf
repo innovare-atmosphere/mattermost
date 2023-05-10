453b0d34b38c72eb9e501064bac7a2fbd96dcb14
Boris Aramis Aguilar Rodríguez <me@borisaguilar.com>
2023-05-10 17:11:43 -0600

    Update dockerbase.tf

--- atmosphere/dockerbase.tf
+++ atmosphere/dockerbase.tf
@@ -78,7 +78,7 @@ resource "digitalocean_droplet" "www-mattermost" {
       "export PATH=$PATH:/usr/bin",
       # run compose
       "cd /root/mattermost",
-      "docker-compose up -d",
+      "docker compose up -d",
       "rm /etc/nginx/sites-enabled/default",
       "systemctl restart nginx",
       "ufw allow http",
